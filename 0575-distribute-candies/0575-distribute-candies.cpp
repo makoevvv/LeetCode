c29f656b3e42e3cde3a469e18ae067e3eac793cc
makoevvv <70099538+makoevvv@users.noreply.github.com>
2025-10-17 18:10:20 +0300

    Time: 236 ms (5.17%), Space: 123.1 MB (25.29%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        set <int> st;
+        for (auto i : candyType) {
+            st.insert(i);
+        }
+        return min(st.size(), candyType.size() / 2);
+    }
+};
